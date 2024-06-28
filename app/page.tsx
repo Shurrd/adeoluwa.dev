517b99050f69d65733c8e9b6e8ee74cfa5c9f6ae
shurrd <engraaadeyemi@gmail.com>
2024-06-28 13:06:09 +0100

    feat:sparkles-text

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Hero from './components/Hero';
+import SparklesText from '@/components/ui/sparkles-text';
 
 const Page = () => {
   return (
     <div className='h-[40rem] w-full rounded-md dark:bg-neutral-950 relative flex flex-col items-center justify-center antialiased'>
-      <div className=''>
-        <Hero />
+      <div className='max-w-2xl mx-auto p-4'>
+        <SparklesText text='Join the Waitlist' />
       </div>
     </div>
   );
